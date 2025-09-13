bdfff3db18eac85b8eb358d156d7c21089d842b1
tomasvera17 <tomasvera17@gmail.com>
2025-09-13 15:25:40 -0300

    cración de app diagnostico con funcionalidades asignar, evaluar y listar

--- config/urls.py
+++ config/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('login.urls')),
     path('recepcion/', include('recepcion.urls')),
+    path('diagnostico/', include('diagnostico.urls')),
 ]
