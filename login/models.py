d9ad0fe498b8f6a13fc302b0ed67fe1ba4dd18cf
tomasvera17 <tomasvera17@gmail.com>
2025-10-20 22:10:20 -0300

    creacion de models y forms app login

--- login/models.py
+++ login/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class Usuario(AbstractUser):
+    TIPO_USUARIO = [
+        ('tecnico', 'Técnico'),
+        ('admin', 'Administrador'),
+    ]
+    tipo_usuario = models.CharField(max_length=10, choices=TIPO_USUARIO, default='admin')
+    telefono = models.CharField(max_length=15, blank=True)
+    direccion = models.TextField(blank=True)
+    def __str__(self):
+        return f"{self.username} ({self.tipo_usuario})"
