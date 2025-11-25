0c160156774070818f8dd18ebbb9bc381d0777e2
CMPunk2004 <antoniobenedetti380@gmail.com>
2025-11-25 14:59:52 -0300

    actualizacion para integrar bd con vistas y templates de login

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import vista_login, cerrar_sesion
+from .views import vista_login, cerrar_sesion, registro_usuario
 
 urlpatterns = [
     path('', vista_login, name= 'login'),
     path('logout/', cerrar_sesion, name= 'logout'),
+    path('registro/', registro_usuario, name='registro_usuario'),
 ]
